52e39aefd70585e54cb9341fa0c8bcdcd552e78f
Jesus Ibanez <jibanez@griddynamics.com>
2023-08-09 23:27:01 -0600

    something

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -2,6 +2,15 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const exec = require('@actions/exec');
 function run() {
+  // 1) get some input values
+  const bucket = core.getInput('bucket', { required: true });
+  const bucketRegion = core.getInput('bucket-region', { required: true });
+  const distFolder = core.getInput('dist-folder', { required: true });
+  console.log(bucket, bucketRegion, distFolder);
+
+  // upload files
+
+
   core.notice('hello from my custom JS action!');
 }
 run();
